a7c6957924d92b7a633a1bd36dd4f87199b2d42b
Michelangelo <Michelangelo@DESKTOP-C01FU0U>
2019-11-24 18:02:41 +0100

    Add project files.

---
+++ LibreriaArray/GestioneArray.cs
@@ -0,0 +1,146 @@
+﻿using System;
+
+namespace LibreriaArray
+{
+    public class GestioneArray
+    {
+        public static void LeggiArrayInt(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"Inserisci il valore dell'array i posizione {1}");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+        }
+        public static void StampaArrayInt(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+        public static int[] CopiaArrayInt(int[] array)
+        {
+            int[] array2 = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array2[i] = array[i];
+            }
+            return array2;
+        }
+        public static bool RicercaArrayInt(int[] array, int elemento)
+        {
+            bool trovato = false;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == elemento)
+                    trovato = true;
+            }
+            return trovato;
+        }
+        public static bool SonoUgualiInt(int[] array1, int[] array2)
+        {
+            if (array1.Length != array2.Length)
+                return false;
+            bool risposta = true;
+            for (int i = 0; i < array1.Length; i++)
+            {
+                if (array1[i] != array2[i])
+                    risposta = false;
+            }
+            return risposta;
+        }
+        public static void LeggiArrayDouble(double[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"Inserisci il valore dell'array i posizione {1}");
+                array[i] = double.Parse(Console.ReadLine());
+            }
+        }
+        public static void StampaArrayDouble(double[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+        public static double[] CopiaArrayDouble(double[] array)
+        {
+            double[] array2 = new double[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array2[i] = array[i];
+            }
+            return array2;
+        }
+        public static bool RicercaArrayDouble(double[] array, double elemento)
+        {
+            bool trovato = false;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == elemento)
+                    trovato = true;
+            }
+            return trovato;
+        }
+        public static bool SonoUgualiDouble(double[] array1, double[] array2)
+        {
+            if (array1.Length != array2.Length)
+                return false;
+            bool risposta = true;
+            for (int i = 0; i < array1.Length; i++)
+            {
+                if (array1[i] != array2[i])
+                    risposta = false;
+            }
+            return risposta;
+        }
+        public static void LeggiArrayString(string[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"Inserisci il valore dell'array i posizione {1}");
+                array[i] = Console.ReadLine();
+            }
+        }
+        public static void StampaArrayString(string[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+        public static string[] CopiaArrayString(string[] array)
+        {
+            string[] array2 = new string[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array2[i] = array[i];
+            }
+            return array2;
+        }
+        public static bool RicercaArrayString(string[] array, string elemento)
+        {
+            bool trovato = false;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == elemento)
+                    trovato = true;
+            }
+            return trovato;
+        }
+        public static bool SonoUgualiString(string[] array1, string[] array2)
+        {
+            if (array1.Length != array2.Length)
+                return false;
+            bool risposta = true;
+            for (int i = 0; i < array1.Length; i++)
+            {
+                if (array1[i] != array2[i])
+                    risposta = false;
+            }
+            return risposta;
+        }
+    }
+}
